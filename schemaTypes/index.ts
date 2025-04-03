be920f156385d89dc714402f8d959c5e6b04702a
Joakim <villokodehode@gmail.com>
2025-04-03 18:56:06 +0200

    forgot to add lore.ts to schemaTypes

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import item from "../schemas/item";
 import character from "../schemas/character";
 import characterClass from "../schemas/characterClass";
 import characterRace from "../schemas/characterRace";
+import lore from "../schemas/lore";
 
-export const schemaTypes = [item, character,characterClass, characterRace]
+export const schemaTypes = [item, character,characterClass, characterRace, lore]
